dcca7092aa14464e57b56a386cedec86af86e8aa
ol222hf <ol222hf@student.lnu.se>
2021-03-11 13:10:18 +0100

    refactor

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import logger from 'morgan'
+
+import { router } from './routes/router.js'
 
 const app = express()
 
 app.use(helmet())
 app.use(cors())
+app.use(logger('dev'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use('/', router)
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening at http://localhost:${process.env.PORT}`)
-})
+    console.log(`Press ctrl + C to terminate...`)
+})  
