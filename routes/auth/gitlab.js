ead0635f83a7bf23269bf36b12ff63169490fb38
ol222hf <ol222hf@student.lnu.se>
2021-03-11 23:36:01 +0100

    auth routes setup

--- routes/auth/gitlab.js
+++ routes/auth/gitlab.js
@@ -1,6 +1,12 @@
 import express from 'express'
-// import createError from 'http-errors'
+import passport from 'passport'
+
+import { GitLabController } from '../../controllers/gitlab-controller.js'
 
 export const router = express.Router()
 
-router.get('/login', (req, res, next) => res.send('We loggin in bois'))
+const controller = new GitLabController()
+
+router.get('/', passport.authenticate('gitlab'))
+
+router.get('/callback', passport.authenticate('gitlab'), (req, res) => { res.redirect('/') })
