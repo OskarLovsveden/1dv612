2c235edbf92724dc7a84b9319ca024105e9c268e
ol222hf <ol222hf@student.lnu.se>
2021-03-13 01:14:01 +0100

    setting up client socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import io from 'socket.io-client'
-const socket = io()
+import { io } from 'socket.io-client'
+// const socket = io(process.env.REACT_APP_SERVER_URL)
+const socket = io('http://localhost:8000')
 
 const App = () => {
+  console.log(socket.id)
+
   return (
     <div className="App">
       Hello World!
--- server/server.js
+++ server/server.js
@@ -4,14 +4,26 @@ import cors from 'cors'
 import helmet from 'helmet'
 import logger from 'morgan'
 import passport from 'passport'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 import { router } from './routes/router.js'
 import { passportInit } from './helpers/passport-gitlab.js'
 
 const app = express()
 
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CLIENT_ORIGIN
+    }
+})
+
+
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: process.env.CLIENT_ORIGIN
+}))
 app.use(logger('dev'))
 
 app.use(session({
@@ -25,8 +37,13 @@ app.use(passport.session())
 passportInit()
 
 app.use('/', router)
+app.set('io', io)
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`Example app listening at http://localhost:${process.env.PORT}`)
     console.log(`Press ctrl + C to terminate...`)
-})  
+})
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+});
