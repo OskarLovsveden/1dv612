a62248a322e03fb6a619916c32eb5529dea2da3e
ol222hf <ol222hf@student.lnu.se>
2021-03-21 22:27:48 +0100

    check if logged in on load

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
-import { io } from 'socket.io-client'
-// const socket = io(process.env.REACT_APP_SERVER_URL)
-const socket = io('http://localhost:8000')
+import { useEffect, useState } from 'react'
+import { Button } from '@chakra-ui/react'
+
+import axios from 'axios'
 
 const App = () => {
-  console.log(socket.id)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const res = await axios('/auth/gitlab/success', {
+        withCredentials: true,
+        baseURL: process.env.REACT_APP_SERVER_URL
+      })
+
+      res.status === 200 ? setUser(res.data.user) : setUser({})
+    }
+
+    checkAuth()
+  }, [])
+
+  const login = () => {
+    window.open(process.env.REACT_APP_SERVER_URL + '/auth/gitlab', '_self')
+  }
+
+  const logout = () => {
+    window.open(process.env.REACT_APP_SERVER_URL + '/auth/gitlab/logout', '_self')
+    setUser(null)
+  }
 
   return (
     <div className="App">
-      Hello World!
+      {user
+        ? <Button onClick={logout}>Logout</Button>
+        : <Button onClick={login}>Login</Button>
+      }
     </div>
   )
 }
