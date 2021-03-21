ce8990f631cf3d21219eaae46cb25a5f7144ad28
ol222hf <ol222hf@student.lnu.se>
2021-03-21 22:28:09 +0100

    cleanup and removing socket

--- server/server.js
+++ server/server.js
@@ -4,46 +4,39 @@ import cors from 'cors'
 import helmet from 'helmet'
 import logger from 'morgan'
 import passport from 'passport'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
 
 import { router } from './routes/router.js'
-import { passportInit } from './helpers/passport-gitlab.js'
+import { setupPassport } from './config/passport-gitlab.js'
+import { connectDB } from './config/mongoose.js'
+
+await connectDB()
 
 const app = express()
 
-const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: process.env.CLIENT_ORIGIN
-    }
-})
-
+app.use(cors({
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true,
+    methods: 'GET'
+}))
 
 app.use(helmet())
-app.use(cors({
-    origin: process.env.CLIENT_ORIGIN
-}))
 app.use(logger('dev'))
+app.use(express.json())
 
 app.use(session({
     secret: 'secrets',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 }
 }))
 
 app.use(passport.initialize())
 app.use(passport.session())
-passportInit()
+setupPassport()
 
 app.use('/', router)
-app.set('io', io)
 
-server.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Example app listening at http://localhost:${process.env.PORT}`)
     console.log(`Press ctrl + C to terminate...`)
 })
-
-io.on('connection', (socket) => {
-    console.log(socket.id)
-});
