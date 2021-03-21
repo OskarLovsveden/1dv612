bc39dfef0f0e1734e793c88a438cd6270a436bee
ol222hf <ol222hf@student.lnu.se>
2021-03-21 22:27:34 +0100

    auth routes refactoring

--- server/routes/auth/gitlab.js
+++ server/routes/auth/gitlab.js
@@ -9,4 +9,29 @@ const controller = new GitLabController()
 
 router.get('/', passport.authenticate('gitlab'))
 
-router.get('/callback', passport.authenticate('gitlab'), (req, res) => { res.redirect('/') })
+router.get('/callback', passport.authenticate('gitlab', {
+    successRedirect: process.env.CLIENT_ORIGIN,
+    failureRedirect: '/auth/gitlab/failed'
+}))
+
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect(process.env.CLIENT_ORIGIN)
+})
+
+router.get('/success', (req, res) => {
+    if (req.user) {
+        res.json({
+            success: true,
+            message: 'user has successfully authenticated',
+            user: req.user
+        })
+    }
+})
+
+router.get('/failed', (req, res) => {
+    res.status(401).json({
+        success: false,
+        message: 'user failed to authenticate.'
+    })
+})
--- server/routes/router.js
+++ server/routes/router.js
@@ -4,7 +4,5 @@ import { router as authRouter } from './auth/router.js'
 
 export const router = express.Router()
 
-router.get('/', (req, res) => req.user ? res.send('Authorized') : res.send('Unauthorized'))
-
 router.use('/auth', authRouter)
 router.use('*', (req, res, next) => next(createError(404)))
