c6eabc2c5a1162f1c334c6c7f714290cabfd709b
ol222hf <ol222hf@student.lnu.se>
2021-03-23 00:55:45 +0100

    wrapping app in router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ChakraProvider } from "@chakra-ui/react"
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -8,7 +9,9 @@ import reportWebVitals from './reportWebVitals'
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
