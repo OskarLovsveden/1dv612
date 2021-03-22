a43df92c37a87427e9862b5e3ff942a6c3d6127f
ol222hf <ol222hf@student.lnu.se>
2021-03-23 00:55:36 +0100

    adding components and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,64 @@
 import { useEffect, useState } from 'react'
-import { Button } from '@chakra-ui/react'
+import {
+  Switch,
+  Route,
+  useLocation
+} from 'react-router-dom'
 
 import axios from 'axios'
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState()
+  const location = useLocation()
 
   useEffect(() => {
-    const checkAuth = async () => {
-      const res = await axios('/auth/gitlab/success', {
-        withCredentials: true,
-        baseURL: process.env.REACT_APP_SERVER_URL
-      })
-
-      res.status === 200 ? setUser(res.data.user) : setUser({})
-    }
-
-    checkAuth()
+    // checkAuth()
   }, [])
 
+  const checkAuth = async () => {
+    const res = await axios('/auth/gitlab/check', {
+      withCredentials: true,
+      baseURL: process.env.REACT_APP_SERVER_URL
+    })
+    res.data.user ? setUser(res.data.user) : setUser(null)
+  }
+
   const login = () => {
-    window.open(process.env.REACT_APP_SERVER_URL + '/auth/gitlab', '_self')
+    window.open(`${process.env.REACT_APP_SERVER_URL}/auth/gitlab`, '_self')
   }
 
   const logout = () => {
-    window.open(process.env.REACT_APP_SERVER_URL + '/auth/gitlab/logout', '_self')
+    window.open(`${process.env.REACT_APP_SERVER_URL}/auth/gitlab/logout`, '_self')
     setUser(null)
   }
 
   return (
     <div className="App">
-      {user
-        ? <Button onClick={logout}>Logout</Button>
-        : <Button onClick={login}>Login</Button>
-      }
-    </div>
+      {location.pathname !== '/login' && <Nav />}
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
+    </div >
   )
 }
+
+const Nav = () => {
+  return <div>NAV</div>
+}
+
+const Login = () => {
+  return <div>LOGIN</div>
+}
+
+const Main = () => {
+  return <div>MAIN</div>
+}
+
+const NotFound = () => {
+  return <div>404 - <code>{window.location.pathname}</code> is not a valid page</div>
+}
+
 
 export default App
