3a1b1bccb9b3ead7cc4ff1078b0bd718ac2467ae
ol222hf <ol222hf@student.lnu.se>
2021-03-23 12:09:42 +0100

    Adding public route

---
+++ client/src/components/routes/PublicRoute.js
@@ -0,0 +1,18 @@
+import { Route, Redirect } from 'react-router-dom'
+import routes from '../../utils/routes.js'
+
+const PublicRoute = (props) => {
+    const { component: Component, restricted = false, isAuthenticated, ...rest } = props
+
+    const render = (props) => {
+        if (isAuthenticated && restricted) {
+            return <Redirect to={routes.DASHBOARD} />
+        }
+
+        return <Component {...props} />
+    }
+
+    return <Route {...rest} render={render} />
+}
+
+export default PublicRoute
