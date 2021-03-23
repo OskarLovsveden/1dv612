196790c45c5e531f0245560f02fc7768eaad0900
ol222hf <ol222hf@student.lnu.se>
2021-03-23 13:21:41 +0100

    request groups with useEffect

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react'
 import { Button } from '@chakra-ui/react'
 
+import axios from 'axios'
+
 const Dashboard = () => {
+    const [groups, setGroups] = useState()
+
+    useEffect(() => {
+        getGroups()
+    }, [])
+
+    const getGroups = async () => {
+        const res = await axios('/api/gitlab/groups', {
+            withCredentials: true,
+            baseURL: process.env.REACT_APP_SERVER_URL
+        })
+
+        console.log(res.data)
+    }
+
     const logout = () => {
         window.open(`${process.env.REACT_APP_SERVER_URL}/auth/gitlab/logout`, '_self')
         // setUser(null)
