e9ee642f81a437e57a5034d307589a0d9b0f0472
ol222hf <ol222hf@student.lnu.se>
2021-03-23 13:51:02 +0100

    get users groups

---
+++ server/routes/gitlab-api/gitlab.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import axios from 'axios'
+
+// import { GitLabApiController as Controller } from '../../controllers/gitlab-api-controller.js'
+
+export const router = express.Router()
+
+// const controller = new Controller()
+
+router.get('/groups', async (req, res, next) => {
+    try {
+        const response = await axios('https://gitlab.lnu.se/api/v4/groups?min_access_level=40', {
+            headers: {
+                'Authorization': 'Bearer ' + req.user.token
+            }
+        })
+
+        const groups = response.data.map(group => ({
+            "id": group.id,
+            "web_url": group.web_url,
+            "name": group.name
+        }))
+
+        res.send(groups)
+
+    } catch (error) {
+        next(error)
+    }
+})
