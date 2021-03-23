fcac855c8b9893757d3af7ff3131cbdb6196338e
ol222hf <ol222hf@student.lnu.se>
2021-03-23 13:22:04 +0100

    main function implemented

--- server/server.js
+++ server/server.js
@@ -9,32 +9,36 @@ import { router } from './routes/router.js'
 import { setupPassport } from './config/passport-gitlab.js'
 import { connectDB } from './config/mongoose.js'
 
-await connectDB()
+const main = async () => {
+    await connectDB()
 
-const app = express()
+    const app = express()
 
-app.use(cors({
-    origin: process.env.CLIENT_ORIGIN,
-    credentials: true
-}))
+    app.use(cors({
+        origin: process.env.CLIENT_ORIGIN,
+        credentials: true
+    }))
 
-app.use(helmet())
-app.use(logger('dev'))
+    app.use(helmet())
+    app.use(logger('dev'))
 
-app.use(session({
-    secret: 'secrets',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60 * 60 * 1000 }
-}))
+    app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 60 * 60 * 1000 }
+    }))
 
-app.use(passport.initialize())
-app.use(passport.session())
-setupPassport()
+    app.use(passport.initialize())
+    app.use(passport.session())
+    setupPassport()
 
-app.use('/', router)
+    app.use('/', router)
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
-    console.log(`Press ctrl + C to terminate...`)
-})
+    app.listen(process.env.PORT, () => {
+        console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+        console.log(`Press ctrl + C to terminate...`)
+    })
+}
+
+main().catch(console.error)
