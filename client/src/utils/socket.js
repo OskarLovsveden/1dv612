00878028b80cbe38abb29cd7011a665f1bf810a3
ol222hf <ol222hf@student.lnu.se>
2021-03-24 21:41:16 +0100

    socket connection method added

---
+++ client/src/utils/socket.js
@@ -0,0 +1,13 @@
+import { io } from 'socket.io-client'
+
+export const connectSocket = (identifier) => {
+    const socket = io(process.env.REACT_APP_SERVER_URL, {
+        query: { identifier: identifier }
+    })
+
+    socket.on('webhook', data => {
+        console.log(data)
+    })
+
+    return socket
+}
