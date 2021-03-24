c207bd5f2dceb39bdcde1f82d2ac34bc5f332aa7
ol222hf <ol222hf@student.lnu.se>
2021-03-24 19:36:12 +0100

    group menu now sets what group to show

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react'
-import { Box, Button, Menu, MenuButton, MenuList, MenuOptionGroup, MenuItemOption } from '@chakra-ui/react'
+import { Box, Button, Menu, MenuButton, MenuList, MenuOptionGroup, MenuItemOption, List, ListItem } from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
+
+import GitLabGroup from './GitLabGroup'
 
 import axios from 'axios'
 
 const Dashboard = () => {
     const [groups, setGroups] = useState()
+    const [selectedGroup, setSelectedGroup] = useState()
 
     useEffect(() => {
         getGroups()
@@ -18,6 +21,12 @@ const Dashboard = () => {
         })
 
         setGroups(res.data)
+        setSelectedGroup(res.data[0])
+    }
+
+    const clickGroup = (name) => {
+        const group = groups.find(g => g.name === name)
+        setSelectedGroup(group)
     }
 
     return (
@@ -27,15 +36,23 @@ const Dashboard = () => {
                     Your Groups
                 </MenuButton>
                 <MenuList minWidth="240px">
-                    {groups && <MenuOptionGroup defaultValue={groups[0].name} type="radio">
-                        {groups.map(g => (
-                            <MenuItemOption value={g.name} key={g.id}>
-                                {g.name}
-                            </MenuItemOption>
-                        ))}
-                    </MenuOptionGroup>}
+                    {
+                        groups &&
+                        <MenuOptionGroup
+                            onChange={(group) => clickGroup(group)}
+                            defaultValue={groups[0].name}
+                            type="radio"
+                        >
+                            {groups.map(g => (
+                                <MenuItemOption value={g.name} key={g.id}>
+                                    {g.name}
+                                </MenuItemOption>
+                            ))}
+                        </MenuOptionGroup>
+                    }
                 </MenuList>
             </Menu>
+            {selectedGroup && <GitLabGroup group={selectedGroup} />}
         </Box>
     )
 }
