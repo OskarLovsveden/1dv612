c3a256c911c6f2852ca0e6aae131800415e4a518
ol222hf <ol222hf@student.lnu.se>
2021-03-25 22:17:29 +0100

    restyled Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,18 @@
-import { Button } from '@chakra-ui/react'
+import { VStack, Center, Image, Button } from '@chakra-ui/react'
 
 const Login = () => {
     const doLogin = () => {
         window.open(`${process.env.REACT_APP_SERVER_URL}/auth/gitlab`, '_self')
     }
 
-    return <Button onClick={doLogin}>LOGIN</Button >
+    return (
+        <VStack>
+            <Image w="sm" src="gitlab.svg" />
+            <Center w="sm">
+                <Button size="lg" onClick={doLogin}>Login</Button >
+            </Center>
+        </VStack>
+    )
 }
 
 export default Login
