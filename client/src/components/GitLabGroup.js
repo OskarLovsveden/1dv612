a7dab946b17980459654a42b70cfa89bc2b51cc8
ol222hf <ol222hf@student.lnu.se>
2021-03-25 22:17:46 +0100

    List is now Accordion

--- client/src/components/GitLabGroup.js
+++ client/src/components/GitLabGroup.js
@@ -1,16 +1,29 @@
-import { List, ListItem } from '@chakra-ui/react'
+import { Accordion, AccordionItem, AccordionButton, Box, AccordionIcon, AccordionPanel } from '@chakra-ui/react'
 
 const GitLabGroup = (props) => {
     const { group } = props
 
     return (
-        <List spacing={3}>
+        <Accordion mt="1" allowToggle allowMultiple>
             {
                 group.projects.map(p => (
-                    p.issues.map(i => <ListItem key={i.id}>{i.title}</ListItem>)
-                )).flat()
+                    p.issues.map(i => (
+                        <AccordionItem key={i.id}>
+                            <h2>
+                                <AccordionButton>
+                                    <Box flex="1" textAlign="left">
+                                        New issue <Box textDecor="underline" as="span">{i.title}</Box>
+                                    </Box>
+                                    <AccordionIcon />
+                                </AccordionButton>
+                            </h2>
+                            <AccordionPanel pb={4}>
+                                {i.description}
+                            </AccordionPanel>
+                        </AccordionItem>
+                    )))).flat()
             }
-        </List>
+        </Accordion>
     )
 }
 
