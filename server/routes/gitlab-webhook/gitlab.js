1e48c312ada4dff1fcb8e05a031440de06bb2497
ol222hf <ol222hf@student.lnu.se>
2021-03-25 22:16:56 +0100

    apply helper to add hook

--- server/routes/gitlab-webhook/gitlab.js
+++ server/routes/gitlab-webhook/gitlab.js
@@ -1,16 +1,30 @@
 import express from 'express'
+import createError from 'http-errors'
+
 import { connections } from '../../utils/socket.js'
+import AxiosHelper from '../../utils/AxiosHelper.js'
 
 // import { GitLabWebhookController as Controller } from '../../controllers/gitlab-webhook-controller.js'
 
 export const router = express.Router()
-
 // const controller = new Controller()
+const axios = new AxiosHelper()
+
 
 router.post('/:project', async (req, res, next) => {
-    console.log('add hook to project: ' + req.params.project)
-    // console.log('for user: ' + req?.user?.token)
-    res.sendStatus(200)
+    try {
+        if (!req.user) {
+            next(createError(403))
+            return
+        }
+
+        const response = await axios.setHook(req.params.project, req.user.token)
+        console.log(response.data)
+
+        res.sendStatus(200)
+    } catch (error) {
+        next(error)
+    }
 })
 
 router.post('/', async (req, res, next) => {
