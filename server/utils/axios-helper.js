01ec54664305a277fd1475594aea57a315b9ffd2
ol222hf <ol222hf@student.lnu.se>
2021-03-26 23:42:09 +0100

    renamed and no longer a class

---
+++ server/utils/axios-helper.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+/**
+ * Get request.
+ *
+ * @param {string} path - The path appended to the base url.
+ * @param {string} token - The bearer token.
+ * @param {string} [baseUrl] - The base url of the request.
+ * @return {Promise} The response of the request as a promise.
+ */
+export const get = async (path, token, baseUrl) => {
+    const url = baseUrl ? baseUrl + path : path
+
+    const response = await axios(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    return response
+}
+
+export const setHook = async (groupID, token) => {
+    const url = `${process.env.GITLAB_API_BASE_URL}/groups/${groupID}/hooks`
+
+    const params = new URLSearchParams()
+    params.append('url', 'https://17015c7393bc.ngrok.io/webhook/gitlab')
+    params.append('token', process.env.GITLAB_WEBHOOK_TOKEN)
+    params.append('issues_events', true)
+
+    const response = await axios(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        data: params
+    })
+
+    return response
+}
