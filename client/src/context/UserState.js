6ef734a58f7b2a781846841dab99a915526b113a
ol222hf <ol222hf@student.lnu.se>
2021-03-26 23:45:41 +0100

    auth/user context added

---
+++ client/src/context/UserState.js
@@ -0,0 +1,51 @@
+import { useEffect, createContext, useReducer } from 'react'
+import UserReducer from './UserReducer'
+import axios from 'axios'
+
+const initialState = {
+    user: null
+}
+
+export const AuthContext = createContext(initialState)
+
+export const AuthProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(UserReducer, initialState)
+
+    useEffect(() => {
+        checkForUser()
+    }, [])
+
+    const checkForUser = async () => {
+        const res = await axios('/auth/gitlab/check', {
+            withCredentials: true,
+            baseURL: process.env.REACT_APP_SERVER_URL
+        })
+
+        if (res.data) {
+            login(res.data)
+        }
+    }
+
+    const isAuthenticated = () => {
+        return state.user ? true : false
+    }
+
+    const login = (user) => {
+        dispatch({
+            type: 'USER_LOGIN',
+            payload: user
+        })
+    }
+
+    return (
+        <AuthContext.Provider
+            value={{
+                user: state.user,
+                isAuthenticated,
+                login
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
