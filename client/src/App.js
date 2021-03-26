4f1f2d183ba619d1d40d5779f15bbf6bf7d34cb0
ol222hf <ol222hf@student.lnu.se>
2021-03-26 15:39:08 +0100

    Moving authentication to context

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 import { Switch, useLocation } from 'react-router-dom'
-import { Container, Spinner } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
 
 import routes from './utils/routes.js'
 import PrivateRoute from './components/routes/PrivateRoute.js'
@@ -10,45 +10,20 @@ import Login from './components/Login.js'
 import NotFound from './components/NotFound.js'
 import Dashboard from './components/Dashboard.js'
 
-import { connectSocket } from './utils/socket.js'
-import axios from 'axios'
+import { AuthContext } from './context/UserState.js'
 
 const App = () => {
-  const [isAuthenticated, setAuthenticated] = useState(false)
-  const [loading, setLoading] = useState(true)
+  const { isAuthenticated } = useContext(AuthContext)
   const location = useLocation()
-
-  useEffect(() => {
-    checkAuthentication()
-  }, [])
-
-  const checkAuthentication = async () => {
-    const res = await axios('/auth/gitlab/check', {
-      withCredentials: true,
-      baseURL: process.env.REACT_APP_SERVER_URL
-    })
-
-    // TODO - Save user in context
-    if (res.data) {
-      setAuthenticated(true)
-      connectSocket(res.data.gitlab_id)
-    } else {
-      setAuthenticated(false)
-    }
-
-    setLoading(false)
-  }
 
   return (
     <Container padding={1}>
       {location.pathname !== routes.LOGIN && <Nav />}
-      {loading
-        ? <Spinner />
-        : <Switch>
-          <PrivateRoute isAuthenticated={isAuthenticated} component={Dashboard} path={routes.DASHBOARD} exact />
-          <PublicRoute isAuthenticated={isAuthenticated} restricted component={Login} path={routes.LOGIN} exact />
-          <PublicRoute isAuthenticated={isAuthenticated} component={NotFound} />
-        </Switch>}
+      <Switch>
+        <PrivateRoute isAuthenticated={isAuthenticated()} component={Dashboard} path={routes.DASHBOARD} exact />
+        <PublicRoute isAuthenticated={isAuthenticated()} restricted component={Login} path={routes.LOGIN} exact />
+        <PublicRoute isAuthenticated={isAuthenticated()} component={NotFound} />
+      </Switch>
     </Container >
   )
 }
