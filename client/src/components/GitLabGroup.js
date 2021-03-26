eac0fb4bc53185bde958b2c3069b276d0c694137
ol222hf <ol222hf@student.lnu.se>
2021-03-26 23:44:48 +0100

    check if issue is newer than last logout

--- client/src/components/GitLabGroup.js
+++ client/src/components/GitLabGroup.js
@@ -1,7 +1,28 @@
-import { Accordion, AccordionItem, AccordionButton, Box, AccordionIcon, AccordionPanel } from '@chakra-ui/react'
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    Box,
+    AccordionIcon,
+    AccordionPanel
+} from '@chakra-ui/react'
+
+import { useContext } from 'react'
+import { AuthContext } from '../context/UserState'
+
+import * as date from '../utils/date-helper.js'
 
 const GitLabGroup = (props) => {
+    const { user } = useContext(AuthContext)
     const { group } = props
+
+    // const addHook = async () => {
+    //     await axios(`/webhook/gitlab/${group.id}`, {
+    //         method: 'POST',
+    //         withCredentials: true,
+    //         baseURL: process.env.REACT_APP_SERVER_URL
+    //     })
+    // }
 
     return (
         <Accordion mt="1" allowToggle allowMultiple>
@@ -14,7 +35,10 @@ const GitLabGroup = (props) => {
                                     <Box flex="1" textAlign="left">
                                         Issue
                                         <Box textDecor="underline" as="span" ml="2">{i.title}</Box>
-                                        {i.new && <Box as="span" ml="2" color="green.300">NEW</Box>}
+                                        {
+                                            date.isNewer(i.updated_at, user.last_login) &&
+                                            <Box as="span" ml="2" color="green.300">NEW</Box>
+                                        }
                                     </Box>
                                     <AccordionIcon />
                                 </AccordionButton>
