02ae3e43dd2013955bd0ef9b70240eb60e21e125
ol222hf <ol222hf@student.lnu.se>
2021-03-26 10:37:55 +0100

    no more hardcoded identifier

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,8 +2,7 @@ import { io } from 'socket.io-client'
 
 export const connectSocket = (identifier) => {
     const socket = io(process.env.REACT_APP_SERVER_URL, {
-        query: { identifier: '123' }
-        // query: { identifier: identifier }
+        query: { identifier: identifier }
     })
 
     socket.on('webhook', data => {
