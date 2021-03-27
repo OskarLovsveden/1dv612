127cb334538d7f0b8550258ac640dcab340f986d
ol222hf <ol222hf@student.lnu.se>
2021-03-28 00:23:13 +0100

    cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ChakraProvider } from "@chakra-ui/react"
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import { ChakraProvider } from "@chakra-ui/react"
 import { AuthProvider } from './context/AuthState'
 
 import App from './App'
