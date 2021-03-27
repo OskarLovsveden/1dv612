6c73fe574bb1da0a03038195b2ab42a7753cd7c9
ol222hf <ol222hf@student.lnu.se>
2021-03-27 22:39:38 +0100

    update model with helpers

--- server/models/NotificationSettings.js
+++ server/models/NotificationSettings.js
@@ -3,10 +3,20 @@ const { Schema } = mongoose
 
 const schema = new Schema({
     gitlab_id: String,
-    channel_hook: String,
-    issue: { type: Boolean, default: false }
+    group_id: String,
+    channel_hook: String
 }, {
     versionKey: false
 })
+
+schema.statics.updateSettings = function (id, url) {
+    const filter = { gitlab_id: id }
+    const update = { channel_hook: url }
+
+    return this.findOneAndUpdate(filter, update, {
+        upsert: true,
+        new: true
+    })
+}
 
 export const NotificationSettings = mongoose.model('NotificationSettings', schema)
