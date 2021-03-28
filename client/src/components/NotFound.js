31ed08a3550d81f0bed4f3a083e219de9577206e
ol222hf <ol222hf@student.lnu.se>
2021-03-28 03:03:50 +0200

    go home button added

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,7 +1,20 @@
-import { Box } from '@chakra-ui/react'
+import { useHistory } from 'react-router-dom'
+
+import { Center, Heading, Text, VStack, IconButton } from '@chakra-ui/react'
+import { MdHome } from "react-icons/md"
 
 const NotFound = () => {
-    return <Box>404 - <code>{window.location.pathname}</code> is not a valid page</Box>
+    const history = useHistory()
+
+    return (
+        <Center>
+            <VStack>
+                <Heading>404</Heading>
+                <Text><code>{window.location.pathname}</code> is not a valid page</Text>
+                <IconButton onClick={() => history.push('/')} size="lg" icon={<MdHome />} />
+            </VStack>
+        </Center>
+    )
 }
 
 export default NotFound
