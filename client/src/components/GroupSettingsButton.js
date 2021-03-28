2894973697d571fbe2bc00989324c996ebf5a4d0
ol222hf <ol222hf@student.lnu.se>
2021-03-28 19:23:14 +0200

    switches/state for selecting type of notification

--- client/src/components/GroupSettingsButton.js
+++ client/src/components/GroupSettingsButton.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useContext } from 'react'
+import { useRef, useState, useEffect, useContext } from 'react'
 
 import {
     Drawer,
@@ -15,8 +15,9 @@ import {
     IconButton,
     OrderedList,
     ListItem,
-    Text,
-    Heading
+    FormControl,
+    Heading,
+    Switch
 } from "@chakra-ui/react"
 
 import * as axios from '../utils/axios-helper'
@@ -26,35 +27,80 @@ import { GroupsContext } from '../context/GroupsState'
 
 const GroupSettingsButton = () => {
     const { selectedGroup, selectGroup } = useContext(GroupsContext)
-
     const { isOpen, onOpen, onClose } = useDisclosure()
-    const [webhookUrl, setWebhookUrl] = useState('')
     const btnRef = useRef()
+
+    const [discHook, setDiscHook] = useState()
+    const [notifyIssue, setNotifyIssue] = useState()
+    const [notifyRelease, setNotifyRelease] = useState()
+
+    useEffect(() => {
+        const setState = () => {
+            if (selectedGroup) {
+                setDiscHook(selectedGroup.channel_hook)
+                setNotifyIssue(selectedGroup.notify_issue)
+                setNotifyRelease(selectedGroup.notify_release)
+            }
+        }
+
+        setState()
+    }, [selectedGroup])
+
 
     const addHook = async () => {
         await axios.post(`/webhook/gitlab/${selectedGroup.id}`, {
             options: {
                 // TODO - Add more options (releases, commits, etc.)
                 // Only option and true by default for now
-                issue: true
+                issue: notifyIssue,
+                release: notifyRelease
             },
-            webhookUrl: webhookUrl
+            channel_hook: discHook
         }
         )
 
-        selectGroup({ ...selectedGroup, channel_hook: webhookUrl })
+        selectGroup({
+            ...selectedGroup,
+            channel_hook: discHook,
+            notify_issue: notifyIssue,
+            notify_release: notifyRelease
+        })
         onClose()
     }
 
     const removeHook = async () => {
         await axios.remove(`/webhook/gitlab/${selectedGroup.id}`)
 
-        selectGroup({ ...selectedGroup, channel_hook: '-' })
+        selectGroup({
+            ...selectedGroup, channel_hook: '',
+            notify_issue: false,
+            notify_release: false
+        })
         onClose()
     }
 
     const handleInputChange = (event) => {
-        setWebhookUrl(event.target.value)
+        setDiscHook(event.target.value)
+    }
+
+    const handleSwitchChange = (event) => {
+        switch (event.target.id) {
+            case 'issue':
+                setNotifyIssue(event.target.checked)
+                break
+            case 'release':
+                setNotifyRelease(event.target.checked)
+                break
+            default:
+                break
+        }
+    }
+
+    const openDrawer = () => {
+        setDiscHook(selectedGroup.channel_hook)
+        setNotifyIssue(selectedGroup.notify_issue)
+        setNotifyRelease(selectedGroup.notify_release)
+        onOpen()
     }
 
     return (
@@ -62,7 +108,7 @@ const GroupSettingsButton = () => {
             <IconButton
                 isLoading={!selectedGroup}
                 ref={btnRef}
-                onClick={onOpen}
+                onClick={openDrawer}
                 icon={<SettingsIcon />}
             />
             <Drawer
@@ -75,7 +121,6 @@ const GroupSettingsButton = () => {
                     <DrawerContent>
                         <DrawerCloseButton />
                         <DrawerHeader>Offline Notifications</DrawerHeader>
-
                         <DrawerBody>
                             <FormLabel htmlFor="email-alerts" mb="0">
                                 <OrderedList>
@@ -87,10 +132,33 @@ const GroupSettingsButton = () => {
                                     <ListItem>Press "Copy Webhook URL"</ListItem>
                                 </OrderedList>
                             </FormLabel>
-                            <Input mt="3" onChange={e => handleInputChange(e)} placeholder="Discord webhook here..." />
                             <Heading mt="3" size="sm">Current hook:</Heading>
-                            <Text>{selectedGroup && selectedGroup.channel_hook}</Text>
+                            <Input
+                                mt="3"
+                                onChange={e => handleInputChange(e)}
+                                placeholder={"Discord webhook here..."}
+                                defaultValue={selectedGroup && selectedGroup.channel_hook}
+                            />
+                            <FormControl display="flex" alignItems="center" mt="2">
+                                <Switch
+                                    id="issue"
+                                    mr="3"
+                                    onChange={(e) => handleSwitchChange(e)}
+                                    defaultChecked={selectedGroup && selectedGroup.notify_issue}
+                                />
+                                <FormLabel mb="0">Issue Notifications</FormLabel>
+                            </FormControl>
+                            <FormControl display="flex" alignItems="center" mt="2">
+                                <Switch
+                                    id="release"
+                                    mr="3"
+                                    onChange={(e) => handleSwitchChange(e)}
+                                    defaultChecked={selectedGroup && selectedGroup.notify_release}
+                                />
+                                <FormLabel mb="0">Release Notifications</FormLabel>
+                            </FormControl>
                         </DrawerBody>
+
 
                         <DrawerFooter>
                             <Button variant="outline" mr={3} onClick={removeHook}>Delete</Button>
