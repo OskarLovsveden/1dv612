a3734f0e1c1bd70e122fb307415c85fbf3737b8e
ol222hf <ol222hf@student.lnu.se>
2021-03-28 19:23:39 +0200

    adding issue and release

--- server/models/NotificationSettings.js
+++ server/models/NotificationSettings.js
@@ -5,14 +5,15 @@ const schema = new Schema({
     gitlab_id: String,
     group_id: String,
     channel_hook: String,
-    token: String
+    token: String,
+    issue: { type: Boolean, default: false },
+    release: { type: Boolean, default: false }
 }, {
     versionKey: false
 })
 
-schema.statics.updateSettings = function (id, url, token) {
+schema.statics.updateSettings = function (id, update) {
     const filter = { gitlab_id: id }
-    const update = { channel_hook: url, token: token }
 
     return this.findOneAndUpdate(filter, update, {
         new: true
