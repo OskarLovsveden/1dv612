7838a15090fc30a738e90fd77ad5b426530cdaf4
ol222hf <ol222hf@student.lnu.se>
2021-03-28 19:32:50 +0200

    render releases

--- client/src/components/GitLabGroupList.js
+++ client/src/components/GitLabGroupList.js
@@ -25,6 +25,26 @@ const GitLabGroupList = () => {
                 selectedGroup.projects.map(p => (
                     <Box key={p.id} m="3">
                         <Heading size="sm"> {p.name}</Heading>
+                        {p.releases.map(r => (
+                            <AccordionItem key={r.tag}>
+                                <Heading>
+                                    <AccordionButton>
+                                        <Box flex="1" textAlign="left">
+                                            Release
+                                        <Box textDecor="underline" as="span" ml="2">{r.tag + ' - ' + r.title}</Box>
+                                            {
+                                                date.isNewer(r.updated_at, user.last_login) &&
+                                                <Box as="span" ml="2" color="green.300">NEW</Box>
+                                            }
+                                        </Box>
+                                        <AccordionIcon />
+                                    </AccordionButton>
+                                </Heading>
+                                <AccordionPanel pb={4}>
+                                    {r.description}
+                                </AccordionPanel>
+                            </AccordionItem>
+                        ))}
                         {p.issues.map(i => (
                             <AccordionItem key={i.id}>
                                 <Heading>
