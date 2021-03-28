a01bcf15f3f4bdd66d3dd03aae75087edc2992a4
ol222hf <ol222hf@student.lnu.se>
2021-03-28 15:24:12 +0200

    model update, includes token

--- server/models/NotificationSettings.js
+++ server/models/NotificationSettings.js
@@ -4,14 +4,15 @@ const { Schema } = mongoose
 const schema = new Schema({
     gitlab_id: String,
     group_id: String,
-    channel_hook: String
+    channel_hook: String,
+    token: String
 }, {
     versionKey: false
 })
 
-schema.statics.updateSettings = function (id, url) {
+schema.statics.updateSettings = function (id, url, token) {
     const filter = { gitlab_id: id }
-    const update = { channel_hook: url }
+    const update = { channel_hook: url, token: token }
 
     return this.findOneAndUpdate(filter, update, {
         new: true
